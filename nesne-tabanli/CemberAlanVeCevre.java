06806482fb3d1484d3e34fb04248813e059e8ee3
Berat Erol <beratlore00@gmail.com>
2025-12-30 13:06:03 +0300

    Refactor: Sınıf referansları, kalıtım yapıları ve constructor isimleri senkronize edildi

--- nesne-tabanli/CemberAlanVeCevre.java
+++ nesne-tabanli/CemberAlanVeCevre.java
@@ -1,52 +1,42 @@
 public class CemberAlanVeCevre {
 
-	// Main metot
-	public static void main(String[] args) {
-		
-		// Yaricapi 1 olan cember olusturma
-		BasitCember cember1 = new BasitCember();
-		System.out.println("Yaricapi " + cember1.yaricap + " olan cemberin alanı: " + cember1.getAlan());
-		
-		// Yaricapi 25 olan cember olusturma
-		BasitCember cember2 = new BasitCember(25);
-		System.out.println("Yaricapi " + cember2.yaricap + " olan cemberin alanı: " + cember2.getAlan());
-		
-		// Yaricapi 125 olan cember olusturma
-		BasitCember cember3 = new BasitCember(125);
-		System.out.println("Yaricapi " + cember3.yaricap + " olan cemberin alanı: " + cember3.getAlan());
-		
-		// Cember yaricapi degistirme
-		cember2.yaricap = 100; // veya cember2.setRadius(100);
-		System.out.println("Yaricapi " + cember2.yaricap + " olan cemberin alanı: " + cember2.getAlan());
-	}
-	
-	double yaricap;
-	
-	// Yaricapi 1 olan bir cember yapılandır
-	BasitCember2() {
-		yaricap = 1;
-	}
-	
-	// Yaricapi belirlenebilen bir cember yapılandır
-	BasitCember2(double yaricap){
-		this.yaricap = yaricap;
-	}
-	
-	// Bu cemberin alanını geri döndür
-	double getAlan() {
-		return yaricap * yaricap * Math.PI;
-	}
-	
-	// Bu cevresini alanını geri döndür
-	double getCevre() {
-		return 2 * yaricap * Math.PI;
-	}
-	
-	// Bu cember icin yeni bir yaricap ata
-	void setYaricap (double yeniYaricap) {
-		yaricap = yeniYaricap;
-	}
-	
+    public static void main(String[] args) {
+        
+        // Yaricapi 1 olan cember olusturma
+        BasitCember cember1 = new BasitCember();
+        System.out.println("Yaricapi " + cember1.yaricap + " olan cemberin alanı: " + cember1.getAlan());
+        
+        // Yaricapi 25 olan cember olusturma
+        BasitCember cember2 = new BasitCember(25);
+        System.out.println("Yaricapi " + cember2.yaricap + " olan cemberin alanı: " + cember2.getAlan());
+        
+        // Cember yaricapi degistirme
+        cember2.setYaricap(100); 
+        System.out.println("Yaricapi " + cember2.yaricap + " olan yeni alan: " + cember2.getAlan());
+    }
 }
 
-
+// Yardimci sinif: BasitCember
+class BasitCember {
+    double yaricap;
+    
+    BasitCember() {
+        yaricap = 1;
+    }
+    
+    BasitCember(double yaricap){
+        this.yaricap = yaricap;
+    }
+    
+    double getAlan() {
+        return yaricap * yaricap * Math.PI;
+    }
+    
+    double getCevre() {
+        return 2 * yaricap * Math.PI;
+    }
+    
+    void setYaricap(double yeniYaricap) {
+        yaricap = yeniYaricap;
+    }
+} 
--- nesne-tabanli/SoyutSinifPolimorfizm.java
+++ nesne-tabanli/SoyutSinifPolimorfizm.java
@@ -1,32 +1,25 @@
-
 public class SoyutSinifPolimorfizm {
 
-	public static void main(String[] args) {
-		// Iki geometrik nesne olustur
-		TemelGeometrikSekil geoNesne1 = new TemelGeometrikSekildenCember(5);
-		TemelGeometrikSekil geoNesne2 = new TemelGeometrikSekildenDikdortgen(5, 3);
-		
-		System.out.println("Iki nesne esit alana mi sahip? " + esitAlan(geoNesne1, geoNesne2));
-		
-		// Cemberi goster
-		gosterTemelGeometrikSekil(geoNesne1);
-		// Diktörgeni goster
-		gosterTemelGeometrikSekil(geoNesne2);
-	}
-		
-		// Iki geometrik nesnenin alanını karşılaştıran metot
-		public static boolean esitAlan(TemelGeometrikSekil nesne1,TemelGeometrikSekil nesne2) {
-			return nesne1.getAlan() == nesne2.getAlan();
-		}
-		
-		// Geometrik nesneyi gösteren metot
-		public static void gosterTemelGeometrikSekil(TemelGeometrikSekil nesne) {
-			System.out.println();
-			System.out.println("Alan: " + nesne.getAlan());
-			System.out.println("Cevre: " + nesne.getCevre());
-		}
-		
-	}
-
-
-
+    public static void main(String[] args) {
+        // Polimorfizm: Üst sınıf referansı, alt sınıf nesnesi
+        SoyutGeometrikSekil geoNesne1 = new GeometrikCemberSinifi(5);
+        SoyutGeometrikSekil geoNesne2 = new GeometrikDikdortgenSinifi(5, 3);
+        
+        System.out.println("Iki nesne esit alana mi sahip? " + esitAlan(geoNesne1, geoNesne2));
+        
+        System.out.print("Cember Bilgileri: ");
+        gosterTemelGeometrikSekil(geoNesne1);
+        
+        System.out.print("\nDikdortgen Bilgileri: ");
+        gosterTemelGeometrikSekil(geoNesne2);
+    }
+        
+    // Herhangi bir SoyutGeometrikSekil turunu kabul eder
+    public static boolean esitAlan(SoyutGeometrikSekil nesne1, SoyutGeometrikSekil nesne2) {
+        return nesne1.getAlan() == nesne2.getAlan();
+    }
+    
+    public static void gosterTemelGeometrikSekil(SoyutGeometrikSekil nesne) {
+        System.out.println("Alan: " + nesne.getAlan() + " | Cevre: " + nesne.getCevre());
+    }
+} 
